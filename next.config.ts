c0a11605d5b4d86797e9d06e9b7197937bcf0e17
kenyk7 <kenykalin@gmail.com>
2025-10-22 12:55:55 -0500

    feat: my config base with  eslint, prettier, vscode, scss

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
-import type { NextConfig } from "next";
+import ESLintPlugin from 'eslint-webpack-plugin';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    config.plugins.push(
+      new ESLintPlugin({
+        extensions: ['ts', 'tsx'],
+        emitWarning: dev,
+      })
+    );
+    return config;
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
